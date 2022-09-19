80b2114cf95bafb9753212ed8a950449beac34d1
MegaMagicarp <66321765+MegaMagicarp@users.noreply.github.com>
2022-09-19 19:02:02 -0400

    clouds work

--- Assets/02-Mission Demolition/Scripts/FollowCam.cs
+++ Assets/02-Mission Demolition/Scripts/FollowCam.cs
@@ -8,6 +8,7 @@ public class FollowCam : MonoBehaviour
 
     [Header("Set in Inspector")]
     public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero;
 
     [Header("Set Dynamically")]
 
@@ -24,11 +25,16 @@ public class FollowCam : MonoBehaviour
 
         Vector3 destination = POI.transform.position;
 
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
+
         destination = Vector3.Lerp(transform.position, destination, easing);
 
         destination.z = camZ;
 
         transform.position = destination;
+
+        Camera.main.orthographicSize = destination.y + 10;
     }
     // Start is called before the first frame update
     void Start()
